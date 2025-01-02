4c5fc6f667b43b6c54e76057de4c506c172449af
Jmint <jeffreymintah737@gmail.com>
2025-01-02 09:40:13 +0000

    changes

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ type Reciper struct {
 	Name string `json:"name"`
 	Tags []string `json:"tags"`
 	Ingredients []string `json:"ingredients"`
+	Instructions []string `json:"instructions"`
+	PublishedAt time.Time `json:"publishedAt"`
 }
 
 func main () {
